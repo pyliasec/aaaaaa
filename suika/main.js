ca6dcbde2514d90d5725526c64b46c22f254f86a
pyliasec <pyliasec@gmail.com>
2024-06-27 14:07:24 +0900

    sex

--- suika/main.js
+++ suika/main.js
@@ -4,7 +4,9 @@ var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
-    Wolrd = Matter.World;
+    World = Matter.World,
+    Body = Matter.Body,
+    Events = Matter.Events;
 
 // 엔진 선언
 const engine = Engine.create();
@@ -39,20 +41,24 @@ const gorund = Bodies.rectangle(310, 820, 620, 60, {
 })
 
 const topLine = Bodies.rectangle(310, 150, 620, 2, {
+    // 이벤트 처리를 위해 이름을 지정
+    name : "topLine",
     isStatic : true, // 고정해주는 기능
     isSensor : true, // 충돌은 감지하나 물리엔진은 적용안함
     render : {fillStyle : '#E6B143'} // 색상 지정 
 })
 
 // 벽 배치
-Wolrd.add(world, [leftWall, rightWall, gorund, topLine]);
+World.add(world, [leftWall, rightWall, gorund, topLine]);
 
 Render.run(render);
 Runner.run(engine);
 
 // 현재 과일 값을 저장할 변수 생성
-let ourrentBody = null;
-let ourentFruit = null;
+let currentBody = null;
+let curentFruit = null;
+// 키 조작을 제어하는 변수 생성
+let disableAction =  false;
 
 // 과일 떨어지는 함수 만들기
 function addFruit() {
@@ -71,10 +77,81 @@ function addFruit() {
         restitution : 0.2,
     });
     // 현재 과일값 저장
-    ourrentBody = body;
-    ourentFruit = fruit;
+    currentBody = body;
+    curentFruit = fruit;
 
-    Wolrd.add(world, body);
+    World.add(world, body);
 }
+
+window.onkeydown = (event) => {
+    // 제어 조작 변수가 true 경우 바로 리턴
+    if (disableAction)
+    return;
+
+    switch(event.code) {
+        case "KeyA" :
+            if (currentBody.position.x - curentFruit.radius > 30)
+            Body.setPosition(currentBody, {
+                x: currentBody.position.x - 10,
+                y: currentBody.position.y
+            })
+            break;
+        case "KeyD":
+            if (currentBody.position.x + curentFruit.radius < 590)
+            Body.setPosition(currentBody, {
+                x: currentBody.position.x + 10,
+                y: currentBody.position.y
+            })
+            break;
+        case "KeyS":
+            currentBody.isSleeping = false;
+            disableAction = true;
+            setTimeout(() => {
+                addFruit();
+                disableAction = false;
+            }, 1000);
+            break;
+    }
+}
+
+Events.on(engine, "collisionStart", (event) => {
+    // 콜리전 이벤트 발생시 생기는 모든 오브젝트를 비교
+    event.pairs.forEach(collision => {
+        if (collision.bodyA.index == collision.bodyB.index) {
+
+            const index = collision.bodyA.index;
+
+            // 수박일 경우 처리 안함
+            if( index === FRUITS.length - 1 ) {
+                return;
+            }
+
+            // 충돌이 일어나는 같은 과일 제거
+            World.remove(world, [collision.bodyA, collision.bodyB])
+
+            // 기존 과일에서 1 증가 시킨 값을 저장
+            const newFruit = FRUITS[index + 1];
+            const newBody = Bodies.circle(
+                // 부딪친 위치의 x, y값
+                collision.collision.supports[0].x,
+                collision.collision.supports[0].y,
+                newFruit.radius,
+                {
+                    // 과일 index 저장
+                    index : index + 1,
+                    // 새로운 과일 렌더링
+                    render : { sprite : {texture : `${newFruit.name}.png` }},
+                }
+            );
+
+            World.add(world, newBody);
+        }
+
+        if(!disableAction && (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")) {
+            alert("Game Over");
+            disableAction = true;
+        }
+    });
+})
 
 addFruit();
